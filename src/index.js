08fba012f7afe72c14b558059425bb67013eca3a
Denny Castillo <dennycastillo123@gmail.com>
2022-04-21 11:27:11 -0400

    Se reafactorizo el index.js de la raiz para que funcione con el Nuevo React v18, y se elimino Report Wev Vitals

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';// nuevo para react 18
 
 document.body.dataset.layoutConfig =
   '{"leftSidebarCondensed":false,"darkMode":true, "showRightSidebarOnStart": false}';
 
-ReactDOM.render(
+// usando react 18
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// IMPORTANTE
+// El StrictMode o modo estricto es una funcionalidad de react para
+// poder verficar en buen funcionamentode la app
+// vuelve a verificar 2 veces si el componente funciona correctamente
+// desactivar si no queremos que se llame 2 veces a un endpoint
+// o montar y desmonantar un componete dos veces OK
+
